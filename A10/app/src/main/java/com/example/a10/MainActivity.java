53d1b50f1221b9de5b84450dc1fd53bf6116027f
HMLarsen <hugomarcel91@gmail.com>
2020-05-06 22:45:42 -0300

    finish main for A10

--- A10/app/src/main/java/com/example/a10/MainActivity.java
+++ A10/app/src/main/java/com/example/a10/MainActivity.java
@@ -2,44 +2,39 @@ package com.example.a10;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.res.Configuration;
 import android.graphics.Canvas;
-import android.graphics.drawable.ShapeDrawable;
-import android.graphics.drawable.shapes.OvalShape;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import androidx.appcompat.widget.AppCompatImageView;
 
 public class MainActivity extends Activity implements SensorEventListener {
+
     private SensorManager sensorManager;
     private Sensor accelerometer;
-    private long lastUpdate;
 
-    AnimatedView animatedView = null;
-    ShapeDrawable mDrawable = new ShapeDrawable();
     public static int x;
     public static int y;
+
+    private int orientation = Configuration.ORIENTATION_PORTRAIT;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
         accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-        lastUpdate = System.currentTimeMillis();
-
-        animatedView = new AnimatedView(this);
-        setContentView(animatedView);
+        setContentView(new AnimatedView(this));
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        sensorManager.registerListener(this, accelerometer,
-                SensorManager.SENSOR_DELAY_GAME);
+        sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_STATUS_ACCURACY_LOW);
     }
 
     @Override
@@ -55,27 +50,62 @@ public class MainActivity extends Activity implements SensorEventListener {
     @Override
     public void onSensorChanged(SensorEvent event) {
         if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
-            x -= (int) event.values[0];
-            y += (int) event.values[1];
+            x -= (int) event.values[0] * 10;
+            y += (int) event.values[1] * 10;
+            if (getResources().getConfiguration().orientation != orientation) {
+                x = 0;
+                y = 0;
+            }
         }
     }
 
     public class AnimatedView extends AppCompatImageView {
 
-        static final int width = 50;
-        static final int height = 50;
+        ImageView background;
+        ImageView spaceship;
+        static final int spaceshipHeightWidth = 500;
 
         public AnimatedView(Context context) {
             super(context);
-            mDrawable = new ShapeDrawable(new OvalShape());
-            mDrawable.getPaint().setColor(0xffffAC23);
-            mDrawable.setBounds(x, y, x + width, y + height);
+            background = new ImageView(context);
+            background.setImageResource(R.drawable.wallp);
+
+            spaceship = new ImageView(context);
+            spaceship.setImageResource(R.drawable.spaceship);
         }
 
         @Override
         protected void onDraw(Canvas canvas) {
-            mDrawable.setBounds(x, y, x + width, y + height);
-            mDrawable.draw(canvas);
+            background.getDrawable().setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
+            background.getDrawable().draw(canvas);
+            spaceship.getDrawable().setBounds(x, y, x + spaceshipHeightWidth, y + spaceshipHeightWidth);
+
+            // saiu pela esquerda
+            if (spaceship.getDrawable().getBounds().left < 0) {
+                x = 0;
+                spaceship.getDrawable().getBounds().left = x;
+                spaceship.getDrawable().getBounds().right = spaceshipHeightWidth;
+            }
+            // saiu por cima
+            if (spaceship.getDrawable().getBounds().top < 0) {
+                y = 0;
+                spaceship.getDrawable().getBounds().top = y;
+                spaceship.getDrawable().getBounds().bottom = spaceshipHeightWidth;
+            }
+            // saiu por baixo
+            if (spaceship.getDrawable().getBounds().bottom > canvas.getHeight()) {
+                y = canvas.getHeight() - spaceshipHeightWidth;
+                spaceship.getDrawable().getBounds().top = y;
+                spaceship.getDrawable().getBounds().bottom = canvas.getHeight();
+            }
+            // saiu pela direita
+            if (spaceship.getDrawable().getBounds().left + spaceshipHeightWidth > canvas.getWidth()) {
+                x = canvas.getWidth() - spaceshipHeightWidth;
+                spaceship.getDrawable().getBounds().left = x;
+                spaceship.getDrawable().getBounds().right = canvas.getWidth();
+            }
+
+            spaceship.getDrawable().draw(canvas);
             invalidate();
         }
     }
