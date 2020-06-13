ea8f46a1fd0353bd7aefc574ca09e0aa93305eea
HMLarsen <hugomarcel91@gmail.com>
2020-06-13 12:07:23 -0300

    adicionada mensageria

--- TrabalhoFinal/app/src/main/java/br/com/furb/trabalhofinal/MainActivity.java
+++ TrabalhoFinal/app/src/main/java/br/com/furb/trabalhofinal/MainActivity.java
@@ -1,14 +1,61 @@
 package br.com.furb.trabalhofinal;
 
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button addCollection = findViewById(R.id.addCollection);
+        addCollection.setOnClickListener(btnMessageOnClickListener);
+    }
+
+    private View.OnClickListener btnMessageOnClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            addCollection();
+        }
+    };
+
+    public void addCollection() {
+        // Create a new user with a first and last name
+        Map<String, Object> user = new HashMap<>();
+        user.put("first", "Ada");
+        user.put("last", "Lovelace");
+        user.put("born", 1815);
+
+        // Add a new document with a generated ID
+        db.collection("users")
+                .add(user)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Log.d("coco", "DocumentSnapshot added with ID: " + documentReference.getId());
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w("coco", "Error adding document", e);
+                    }
+                });
     }
 }
