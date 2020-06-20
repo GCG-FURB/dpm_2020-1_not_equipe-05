65db6c1c82d720c531440544a89f79ffee113822
HMLarsen <hugomarcel91@gmail.com>
2020-06-20 18:44:56 -0300

    cria serviço de acesso ao firestore

--- TrabalhoFinal/app/src/main/java/br/com/furb/trabalhofinal/MainActivity.java
+++ TrabalhoFinal/app/src/main/java/br/com/furb/trabalhofinal/MainActivity.java
@@ -4,57 +4,77 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.List;
+
+import br.com.furb.trabalhofinal.model.EventPojo;
+import br.com.furb.trabalhofinal.model.Participant;
+import br.com.furb.trabalhofinal.service.EventService;
 
 public class MainActivity extends AppCompatActivity {
 
-    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private Button addEvent;
+    private TextView eventName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button addCollection = findViewById(R.id.addCollection);
-        addCollection.setOnClickListener(btnMessageOnClickListener);
+
+        addEvent = findViewById(R.id.bAddEvent);
+        eventName = findViewById(R.id.ptEventName);
+
+        addEvent.setOnClickListener(addEventListener);
     }
 
-    private View.OnClickListener btnMessageOnClickListener = new View.OnClickListener() {
+    private View.OnClickListener addEventListener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-            addCollection();
+            addEvent();
         }
     };
 
-    public void addCollection() {
-        // Create a new user with a first and last name
-        Map<String, Object> user = new HashMap<>();
-        user.put("first", "Ada");
-        user.put("last", "Lovelace");
-        user.put("born", 1815);
+    public void addEvent() {
+        EventPojo event = new EventPojo();
+        event.setName(eventName.getText().toString());
 
-        // Add a new document with a generated ID
-        db.collection("users")
-                .add(user)
-                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+        Participant participant1 = new Participant();
+        participant1.setName("teste");
+        Participant participant2 = new Participant();
+        participant2.setName("teste2");
+
+        event.getParticipants().add(participant1);
+        event.getParticipants().add(participant2);
+
+        EventService eventService = new EventService();
+        eventService.put(event)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
                     @Override
-                    public void onSuccess(DocumentReference documentReference) {
-                        Log.d("coco", "DocumentSnapshot added with ID: " + documentReference.getId());
+                    public void onSuccess(Void aVoid) {
                     }
                 })
                 .addOnFailureListener(new OnFailureListener() {
                     @Override
                     public void onFailure(@NonNull Exception e) {
-                        Log.w("coco", "Error adding document", e);
+                    }
+                });
+        eventService.getAll()
+                .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+                    @Override
+                    public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                        if (queryDocumentSnapshots.isEmpty()) {
+                            return;
+                        }
+                        List<EventPojo> events = queryDocumentSnapshots.toObjects(EventPojo.class);
+                        events.forEach(e -> Log.d("teste", e.toString()));
                     }
                 });
     }
