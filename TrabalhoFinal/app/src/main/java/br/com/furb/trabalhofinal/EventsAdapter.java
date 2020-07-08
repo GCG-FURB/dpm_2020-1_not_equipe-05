0cab2dac501d389e6a6967543426b1055685b6f9
HMLarsen <hugomarcel91@gmail.com>
2020-07-08 17:25:23 -0300

    adjusting list

--- TrabalhoFinal/app/src/main/java/br/com/furb/trabalhofinal/EventsAdapter.java
+++ TrabalhoFinal/app/src/main/java/br/com/furb/trabalhofinal/EventsAdapter.java
@@ -1,6 +1,8 @@
 package br.com.furb.trabalhofinal;
 
+import android.content.Intent;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
@@ -14,28 +16,42 @@ import br.com.furb.trabalhofinal.model.EventPojo;
 public class EventsAdapter extends RecyclerView.Adapter<EventsAdapter.MyViewHolder> {
 
     private List<EventPojo> mDataset;
+    private static EventsAdapter activity;
 
     // Provide a reference to the views for each data item
     // Complex data items may need more than one view per item, and
     // you provide access to all the views for a data item in a view holder
     public static class MyViewHolder extends RecyclerView.ViewHolder {
         // each data item is just a string in this case
-        public ConstraintLayout textView;
-        public MyViewHolder(ConstraintLayout v) {
+        public TextView eventName;
+        public TextView eventAddress;
+
+        public MyViewHolder(View v) {
             super(v);
-            textView = v;
+            // Handle item click and set the selection
+            v.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(v.getContext(), Join.class);
+                    intent.putExtra("eventName", eventName.getText().toString());
+                    intent.putExtra("eventAddress", eventAddress.getText().toString());
+                    v.getContext().startActivity(intent);
+                }
+            });
+            eventName = (TextView) v.findViewById(R.id.eventName);
+            eventAddress = (TextView) v.findViewById(R.id.eventAddress);
         }
     }
 
     // Provide a suitable constructor (depends on the kind of dataset)
     public EventsAdapter(List<EventPojo> myDataset) {
         mDataset = myDataset;
+        activity = this;
     }
 
     // Create new views (invoked by the layout manager)
     @Override
-    public EventsAdapter.MyViewHolder onCreateViewHolder(ViewGroup parent,
-                                                     int viewType) {
+    public EventsAdapter.MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         // create a new view
         ConstraintLayout v = (ConstraintLayout) LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.event_list_layout, parent, false);
@@ -48,9 +64,8 @@ public class EventsAdapter extends RecyclerView.Adapter<EventsAdapter.MyViewHold
     public void onBindViewHolder(MyViewHolder holder, int position) {
         // - get element from your dataset at this position
         // - replace the contents of the view with that element
-        TextView eventName = (TextView) holder.textView.getViewById(R.id.eventName);
-        eventName.setText(mDataset.get(position).getDescricao());
-
+        holder.eventName.setText(mDataset.get(position).getDescricao());
+        holder.eventAddress.setText(mDataset.get(position).getEndereco());
     }
 
     // Return the size of your dataset (invoked by the layout manager)
--- TrabalhoFinal/app/src/main/java/br/com/furb/trabalhofinal/Join.java
+++ TrabalhoFinal/app/src/main/java/br/com/furb/trabalhofinal/Join.java
@@ -1,21 +1,48 @@
 package br.com.furb.trabalhofinal;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class Join extends AppCompatActivity {
+
+    private TextView ptEventName;
+    private TextView ptEventDesc;
+    private TextView eventName;
+    private TextView ptEventDate;
+    private TextView ptEventHour;
+    private TextView ptEventAdre;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_join);
 
+        Intent intent = getIntent();
+        String eventName = intent.getStringExtra("eventName");
+        String eventDesc = intent.getStringExtra("eventDesc");
+        String eventDate = intent.getStringExtra("eventDate");
+        String eventHour = intent.getStringExtra("eventHour");
+        String eventAddress = intent.getStringExtra("eventAddress");
+
+        ptEventName = (TextView) findViewById(R.id.ptEventName);
+        ptEventDesc = (TextView) findViewById(R.id.ptEventDesc);
+        ptEventDate = (TextView) findViewById(R.id.ptEventDate);
+        ptEventHour = (TextView) findViewById(R.id.ptEventHour);
+        ptEventAdre = (TextView) findViewById(R.id.ptEventAdre);
+
+        ptEventName.setText(eventName);
+        ptEventDesc.setText(eventDesc);
+        ptEventDate.setText(eventDate);
+        ptEventHour.setText(eventHour);
+        ptEventAdre.setText(eventAddress);
+
         // Botão para retornar para lista de eventos
-        Button BtBackToList = (Button) findViewById(R.id.btBackToList);
+        Button BtBackToList = (Button) findViewById(R.id.btLBackToList);
         BtBackToList.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 Intent it = new Intent(Join.this, Lista.class);
